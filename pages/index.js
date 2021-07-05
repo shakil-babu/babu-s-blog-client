3ccf94da8adc893ecb9bda92088c87848dd028d7
shakil-babu <personal.shakil.babu@gmail.com>
2021-07-05 10:38:04 +0600

    add-getInlineProps

--- pages/index.js
+++ pages/index.js
@@ -71,7 +71,7 @@ export default Home ;
 
 
 // getStaticProps
-export const getStaticProps = async () => {
+Error.getInlineProps = async () => {
   
   // get files from the posts dir
   const files = fs.readdirSync(path.join('posts'));
